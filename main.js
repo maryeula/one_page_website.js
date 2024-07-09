f1eb58d0f89ba60e6f302a69825515ede10380af
maryeula <maryeulagloriani@gmail.com>
2024-07-09 02:51:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex1 = 0;
+showSlides(slideIndex1);
+
+function plusSlides(n) {
+  showSlides(slideIndex1 += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex1 = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex1-1].style.display = "block";
+  dots[slideIndex1-1].className += " active";
+  captionText.innerHTML = dots[slideIndex1-1].alt;
+}
